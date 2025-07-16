5f6ef5a59ae8c5631631d6287ef718bdc6fafc9e
upc <upc@europeia.dev>
2025-07-16 16:59:50 -0400

    feat: init logger

--- cmd/m0use/main.go
+++ cmd/m0use/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"log/slog"
+	"m0use/pkg/config"
+	"os"
+)
+
+func main() {
+	var path string
+
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	} else {
+		path = "config.yml"
+	}
+
+	conf, err := config.ReadConfig(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config.InitLogger(conf)
+
+	slog.Info("starting")
+
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,8 @@ go 1.24.3
 require (
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/nsupc/eurogo v0.2.0 // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	github.com/samber/slog-betterstack v1.4.2 // indirect
+	github.com/samber/slog-common v0.18.1 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
